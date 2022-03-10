2b290dc46cd6089f25635a7d078b291eae5188fd
SmolSwede <samuel_schon@hotmail.com>
2022-03-10 13:52:26 +0100

    Added Ajax for showing all existing people and the abilty to search and delete a person by their ID

---
+++ MVC_Data/Controllers/AjaxController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Data.Services;
+using MVC_Data.Models;
+using MVC_Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Data.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetPeople()
+        {
+            InMemoryPerson inMemoryPerson = new InMemoryPerson();
+            List<Person> peopleList = inMemoryPerson.Read();
+
+            return PartialView("PeopleListPartialView", peopleList);
+        }
+
+        [HttpPost]
+        public IActionResult FindPersonById(int personId)
+        {
+            InMemoryPerson inMemoryPerson = new InMemoryPerson();
+            Person targetPerson = inMemoryPerson.Read(personId);
+            List<Person> people = new List<Person>();
+            if(targetPerson != null)
+            {
+                people.Add(targetPerson);
+            }
+
+            return PartialView("PeopleListPartialView", people);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePersonById(int personId)
+        {
+            InMemoryPerson inMemoryPerson = new InMemoryPerson();
+            Person targetPerson = inMemoryPerson.Read(personId);
+            bool success = false;
+            if(targetPerson != null)
+            {
+                success = inMemoryPerson.Delete(personId);
+            }
+            if (success)
+            {
+                return StatusCode(200);
+            }
+            return StatusCode(404);
+        }
+    }
+}
---
+++ MVC_Data/wwwroot/Scripts/PeopleScript.js
@@ -0,0 +1,25 @@
+﻿function getPeople() {
+    $.get("/Ajax/GetPeople", null, function (data) {
+        $("#PersonList").html(data);
+    });
+}
+
+function getPersonById() {
+    var PersonId = document.getElementById("PersonIdInput").value;
+    $.post("/Ajax/FindPersonById", { personId: PersonId }, function (data) {
+        $("#PersonList").html(data);
+    });
+}
+
+function deletePersonById() {
+    var PersonId = document.getElementById("PersonIdInput").value;
+    $.post("/Ajax/DeletePersonById", { personId: PersonId }, function (data) {
+
+    })
+        .done(function () {
+            document.getElementById("errorMsg").innerHTML = "Successfully deleted Person";
+        })
+        .failk(function () {
+            document.getElementById("errorMsg").innerHTML = "Could not delete Person";
+        });
+}
